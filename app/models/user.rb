5ac930d3613ddf0d22c8c3e6a0c557406af3991a
kevin_garay <kgaray@applaudostudios.dev>
2022-10-19 09:45:55 -0500

    Build message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 15 }, uniqueness: { case_sensitive: false }
   has_secure_password
+  has_many :texts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_142646) do
+ActiveRecord::Schema.define(version: 2022_10_19_144203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "texts", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
