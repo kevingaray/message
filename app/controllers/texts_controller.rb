b44bbdf34577fd0dcdbc28a5f3571efddd7dffd0
kevin_garay <kgaray@applaudostudios.dev>
2022-10-19 19:59:55 -0500

    Display messages using partial!

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -4,7 +4,8 @@ class TextsController < ApplicationController
   def create
     text = current_user.texts.build(text_params)
     if text.save
-      ActionCable.server.broadcast "chatroom_channel", {foo: text.body}
+      ActionCable.server.broadcast "chatroom_channel", {mod_message: message_render(text)}
+      
     end
   end
 
@@ -12,6 +13,10 @@ class TextsController < ApplicationController
 
   def text_params
     params.require(:text).permit(:body)
+  end
+
+  def message_render(text)
+    render(partial: 'text', locals: {text: text})
   end
 
 end
