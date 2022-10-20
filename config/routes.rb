6665dde0bcc1dd7138f64f145d8ef605b7cb3f8e
kevin_garay <kgaray@applaudostudios.dev>
2022-10-19 19:33:24 -0500

    generate chatroom channel

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'text', to: 'texts#create'
+
+  mount ActionCable.server, at: '/cable'
 end
